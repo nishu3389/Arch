7c69b87025362e33c3b18cd20669882760d8792b
Avinash <teamAndroid321>
2020-10-06 15:16:22 +0530

    latest

--- app/src/main/java/com/three/u/ui/tipsandtricks/TipsAndTricksFragment.kt
+++ app/src/main/java/com/three/u/ui/tipsandtricks/TipsAndTricksFragment.kt
@@ -24,6 +24,8 @@ class TipsAndTricksFragment : BaseFragment() {
 
 
 
+
+
     var mealList = arrayListOf<ResponseTipsOuterItem>()
     val mClickHandler: ClickHandler by lazy { ClickHandler() }
     lateinit var mViewModel: TipsAndTricksViewModel
